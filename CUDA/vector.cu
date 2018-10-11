a62d1a5c7074b430da8ff9f0c6095f8a7db2783a
HenriqueDMahl <42521129+HenriqueDMahl@users.noreply.github.com>
2018-10-11 17:01:28 -0300

    Add files via upload

--- CUDA/vector.cu
+++ CUDA/vector.cu
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#define N (2048*2048)
+#include <math.h>
+#define N 1024// 1024 = 32 * 32
+#define TAM 3
 #define THREADS_PER_BLOCK 512
 
 __global__ void soma(float *a, float *b, float *c){
-	int x = threadIdx.x; int y = threadIdx.y;
+	int x = blockIdx.x * blockDim.x + threadIdx.x, y = blockIdx.y * blockDim.y + threadIdx.y;
 	// Identifies the index of the operation(vector)
-	int id = blockIdx.x + x + y;
+	int id = TAM*y + x;
 	// Performs the sum
-	c[id] = a[id] + b[id];
+	if(id < TAM*TAM)
+		c[id] = a[id] + b[id];
 }
 
 __global__ void vector_add(int *a, int *b, int *c) {
@@ -15,26 +18,58 @@ __global__ void vector_add(int *a, int *b, int *c) {
 	if(index < N) c[index] = a[index] + b[index];
 }
 
+int * newMatrix(int flag){
+	int *m = (int *) malloc(sizeof(int)*TAM*TAM);
+	for(int i = 0; i < TAM*TAM; i++)
+		if(!flag)
+			m[i] = 0;
+		else
+			m[i] = 1;
+	return m;
+
+}
+
+void printMatrix(int *m){
+	int cont = 0;
+	for(int i = 0; i< TAM*TAM; i++){
+		if(cont == TAM){
+			cont = 0;
+			printf("\n");
+		}
+		if(cont != TAM){
+			printf("%d ", m[i]);
+			cont++;
+		}
+	}
+	printf("\n");
+}
+
 int main() {
 	int *a, *b, *c;
-	int *d_a, *d_b, *d_c;
+	float *d_a, *d_b, *d_c;
 	int size = N * sizeof( int );
 	cudaMalloc( (void **) &d_a, size );
 	cudaMalloc( (void **) &d_b, size );
 	cudaMalloc( (void **) &d_c, size );
-	a = (int *)malloc( size );
-	b = (int *)malloc( size );
-	c = (int *)malloc( size );
-	for( int i = 0; i < N; i++ ) {
-		a[i] = b[i] = i;
-		c[i] = 0;
-	}
+
+	a = newMatrix(1);
+	b = newMatrix(1);
+	c = newMatrix(0);
+
+	//printMatrix(a);
+
 	cudaMemcpy( d_a, a, size, cudaMemcpyHostToDevice );
 	cudaMemcpy( d_b, b, size, cudaMemcpyHostToDevice );
-	vector_add<<< (N + (THREADS_PER_BLOCK-1)) / THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>( d_a, d_b, d_c );
+
+	//soma<<< (N + ceil(THREADS_PER_BLOCK-1)) / THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>( d_a, d_b, d_c );
+	dim3 dimBlock (TAM + (int)ceil(TAM*TAM/N));
+    dim3 dimGrid (32,32);
+	soma<<< dimBlock,dimGrid >>> ( d_a, d_b, d_c );
 	cudaMemcpy( c, d_c, size, cudaMemcpyDeviceToHost );
-	printf( "c[0] = %d\n",c[0] );
-	printf( "c[%d] = %d\n",N-1, c[N-1] );
+	printMatrix(c);
+	//printf( "c[0] = %d\n",c[0] );
+	//printf( "c[%d] = %d\n",N-1, c[N-1] );
+	//printf("d_a %f\n", *d_a);
 	free(a);
 	free(b);
 	free(c);
